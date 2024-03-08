0403eea55aa564ed0e0153161883295617a9f0f2
andresknoo00001 <32203440+andresknoo00001@users.noreply.github.com>
2024-03-08 14:53:02 -0500

    Prueba tecnica finalizada luego de instalar quasar
    luego de que instale quasar este es el codigo que lleve a cabo para la representacion basica de html y css con sass y variables

---
+++ prueba-2-spl/src/components/PruebaTecnica.vue
@@ -0,0 +1,63 @@
+<template>
+  <section id="prueba-tecnica">
+    <h5 class="title">
+      Prueba tecnica Sprotloud representacion basica de formulario html y css
+    </h5>
+    <section class="formulario">
+      <h6>Formulario de conversiones</h6>
+      <q-form id="conversion-form">
+        <div class="conversion-field">
+          <label for="#term-conversion">Conversion term</label>
+          <q-input
+            requiered
+            id="term-conversion"
+            rounded
+            outlined
+            v-model="text"
+            label="Term conversion"
+          />
+        </div>
+        <div class="description-field">
+          <textarea name="description" id="descrition" rows="10"></textarea>
+        </div>
+      </q-form>
+    </section>
+    <section id="condicionales">
+      <h6>Conditions</h6>
+      <hr>
+      <div class="conditions-group-general-container">
+        <div class="row1 row justify-center space-between">
+          <q-select rounded outlined label="Term policy" class="col-3" name="term policy" id="term-policy"></q-select>
+          <q-select rounded outlined label="is not" class="col-1" name="aplication" id="aplication"></q-select>
+          <q-select rounded outlined label="" class="col-7" name="reason" id="reason"></q-select>
+          <q-icon class="delete" name="close" />
+        </div>
+        <div class="row2 row justify-center space-between">
+          <q-select rounded outlined label="Term policy" class="col-3" name="term policy" id="term-policy"></q-select>
+          <q-select rounded outlined label="is not" class="col-1" name="aplication" id="aplication"></q-select>
+          <q-select rounded outlined label="" class="col-7" name="reason" id="reason"></q-select>
+          <q-icon class="delete" name="close" />
+        </div>
+        <div class="row3 row justify-center space-between">
+          <q-select rounded outlined label="Term policy" class="col-3" name="term policy" id="term-policy"></q-select>
+          <q-select rounded outlined label="is not" class="col-1" name="aplication" id="aplication"></q-select>
+          <q-select rounded outlined label="" class="col-7" name="reason" id="reason"></q-select>
+          <q-icon class="delete" name="close" />
+        </div>
+      </div>
+        <div class="btn-group">
+          <button class="and"> AND </button>
+          <button class or> OR</button>
+        </div>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "PruebaTecnica",
+  setup() {
+    return {};
+  },
+};
+</script>
--- prueba-2-spl/src/pages/IndexPage.vue
+++ prueba-2-spl/src/pages/IndexPage.vue
@@ -1,17 +1,17 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+
+    <prueba/>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-
+import prueba from "components/PruebaTecnica.vue";
 export default defineComponent({
+  components:{
+    prueba
+  },
   name: 'IndexPage'
 })
 </script>
